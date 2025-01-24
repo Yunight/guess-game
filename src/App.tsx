b7d0df43c0635b745f8ac1391ef3fe9dafba3696
Benchi CHEN <benchi.chen.bc@gmail.com>
2025-01-24 09:21:58 +0100

    analytics and perf integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
-import { Provider } from 'react-redux';
-import { store } from './store/store';
-import PokemonGame from '@/components/pokemon-game/PokemonGame';
-import { ResourcePreloader } from '@/components/pokemon-game/ResourcePreloader';
-import { PWAInstallPrompt } from '@/components/ui/PWAInstallPrompt';
-import './App.css';
+import { PWAInstallPrompt } from "@/components/ui/PWAInstallPrompt";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import "./App.css";
+import { Analytics } from "@vercel/analytics/react";
+import PokemonGame from "./components/pokemon-game/PokemonGame";
+import { ResourcePreloader } from "./components/pokemon-game/ResourcePreloader";
 
 function App() {
-  return (
-    <div className="App">
-      <Provider store={store}>
-        <ResourcePreloader>
-          <PokemonGame />
-        </ResourcePreloader>
-      </Provider>
-      <PWAInstallPrompt />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Provider store={store}>
+				<ResourcePreloader>
+					<PokemonGame />
+				</ResourcePreloader>
+				<SpeedInsights />
+				<Analytics />
+			</Provider>
+			<PWAInstallPrompt />
+		</div>
+	);
 }
 
 export default App;
